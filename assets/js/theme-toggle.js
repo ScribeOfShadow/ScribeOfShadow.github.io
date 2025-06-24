1ca3db4a91bc42bb63f64cafccc0efe41f3d3b0a
Solomon <149597774+ScribeOfShadow@users.noreply.github.com>
2025-06-24 19:29:53 +0200

    rework

--- assets/js/theme-toggle.js
+++ assets/js/theme-toggle.js
@@ -1,13 +1,15 @@
 const toggleBtn = document.getElementById('themeToggle');
-const themeLink = document.getElementById('theme-style');
+const themeStyle = document.getElementById('theme-style');
+const body = document.body;
 
 toggleBtn.addEventListener('click', () => {
-  if (themeLink.getAttribute('href').includes('light.css')) {
-    themeLink.setAttribute('href', 'assets/css/dark.css');
-    document.body.className = 'theme-dark';
+  if (themeStyle.getAttribute('href').includes('light.css')) {
+    themeStyle.setAttribute('href', 'assets/css/dark.css');
+    body.className = 'theme-dark fade-transition';
+    toggleBtn.classList.add('dark-mode');
   } else {
-    themeLink.setAttribute('href', 'assets/css/light.css');
-    document.body.className = 'theme-light';
+    themeStyle.setAttribute('href', 'assets/css/light.css');
+    body.className = 'theme-light fade-transition';
+    toggleBtn.classList.remove('dark-mode');
   }
 });
-document.body.classList.add('fade-transition');
